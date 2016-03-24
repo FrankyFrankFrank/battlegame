04cd08614b10ce66553c7fa059c59659279e9af3
afrankhawksview <afrank@hawksviewhomes.com>
2016-03-24 12:39:50 -0400

    First Commit
    Initial Game Files

---
+++ js/battlegame.js
@@ -0,0 +1,64 @@
+$('document').ready(function(){
+	$('body').append("<div>Battle Game</div>");
+
+	var battlegame = {
+
+	Warrior : function ( name, hp, dmg, block ) {
+		this.name = name;
+		this.health = hp;
+		this.damage = dmg;
+		this.block = block;
+	},
+
+	// INITIALIZE
+	init : function(){
+
+		//Set up battle
+		$('body').append("<div id='player1'></div><div id='player2'></div>");
+
+		//Display Health
+		$('#player1').html( player1.name + " - HP:" + player1.health );
+		$('#player2').html( player2.name + " - HP:" + player2.health );
+
+		// Display Attack Buttons
+		$('body').append("<div id='p1attack'>P1 attack</div><div id='p2attack'>P2 attack</div>");
+
+	},
+
+	listeners : function(){
+		$('#start').click(function(){
+			
+		});
+
+		$('#p1attack').click(function(){
+			battlegame.attack(player1,player2);
+		});
+
+		$('#p2attack').click(function(){
+			battlegame.attack(player2,player1);
+		});
+	},
+
+	attack : function( attacker , defender ){
+
+		defender.health = defender.health - (attacker.damage - defender.block);
+
+		console.log(defender.health);
+		battlegame.update();
+	},
+
+	update : function(){
+		$('#player1').html( player1.name + " - HP:" + player1.health );
+		$('#player2').html( player2.name + " - HP:" + player2.health );
+	}
+
+	};
+	//END APP
+
+	var player1 = new battlegame.Warrior("Adam", 100, 65, 4);
+	var player2 = new battlegame.Warrior("Kathleen", 100, 30, 20);
+
+	battlegame.init();
+	battlegame.listeners();
+
+});
